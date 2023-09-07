a2a71e2eeaf7fea2692a6411163e133361ab5fb7
Olvwadamilola <destric3@gmail.com>
2023-09-07 12:18:36 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main -  main function
+ * puts:function
+ *
+ * Return:ends with 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
