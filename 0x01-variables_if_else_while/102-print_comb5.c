f4ada0a91534bbb962fb259bb7af9daac840ddec
Olvwadamilola <destric3@gmail.com>
2023-09-08 16:31:12 -0700

    minor changes to 102-print_comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 
 /**
- * main - this is the entry point of the program
- *
- * Return: always 0
+* main - This is the entry point of the program
+*
+* Return: Always 0
 */
 int main(void)
 {
 	int a, b;
 
 	for (a = 0; a <= 99; a++)
+{
 	for (b = a + 1; b <= 99; b++)
+{
 	if (a % 10 != b % 10 || a / 10 != b / 10)
 {
 	putchar('0' + a / 10);
@@ -18,12 +20,16 @@ int main(void)
 	putchar(' ');
 	putchar('0' + b / 10);
 	putchar('0' + b % 10);
-}
+
 	if (a != 98 || b != 99)
 {
 	putchar(',');
 	putchar(' ');
 }
+}
+}
+}
+
 	putchar('\n');
 
 	return (0);
