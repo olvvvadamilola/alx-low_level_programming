bb1e4c44dcc1b92b8a93daf38680617f4804e486
Olvwadamilola <destric3@gmail.com>
2023-09-08 10:45:10 -0700

    dont forget the capital letters

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -24,7 +24,7 @@ int main(void)
 	}
 	else
 	{
-		printf("last digit of %d is %d and is less than 6 and not 0\n", n, lastNum);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastNum);
 	}
 	return (0);
 }
