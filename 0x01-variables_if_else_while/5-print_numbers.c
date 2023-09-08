e2bcc50d6cf6a7dfe8b7bf6167f2d3156ab15c87
Olvwadamilola <destric3@gmail.com>
2023-09-08 12:06:20 -0700

    dont forget symbols

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - this is the entry point of program
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int number;
+
+	for (number = 0; number <= 9; number++)
+{
+	printf("%d", number);
+}
+	printf("\n");
+
+	return (0);
+}
