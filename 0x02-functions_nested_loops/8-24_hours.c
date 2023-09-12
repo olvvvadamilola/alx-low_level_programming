7a2a125f749343cc87d09b6a59fe6d35decccd11
Olvwadamilola <destric3@gmail.com>
2023-09-12 12:15:48 -0700

    always crosscheck code

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -19,7 +19,7 @@ void jack_bauer(void)
 	_putchar('0' + (hour % 10));
 	_putchar(':');
 	_putchar('0' + (minute / 10));
-	_putchar('0' + (minute / 10));
+	_putchar('0' + (minute % 10));
 	_putchar('\n');
 }
 }
