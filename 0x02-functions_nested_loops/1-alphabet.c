84b4e0f08d188450dc61ba2492d5db9aa25e0179
Olvwadamilola <destric3@gmail.com>
2023-09-12 08:28:43 -0700

    header file with all of today_s prototypes

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* print_alphabet - single line listing of alphabets
+* main - entry point
+* 
+* Return: 0
+*/
+void print_alphabet(void)
+	
---
+++ 0x02-functions_nested_loops/1-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* main - Entry point
+* prints the alphabet in lowercase
+*
+* Return: Always 0.
+*/
+int main(void)
+{
+    print_alphabet();
+    return (0);
+}
