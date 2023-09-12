59f0036c2721c049219ad570f22506f6b37e180b
Olvwadamilola <destric3@gmail.com>
2023-09-12 07:40:15 -0700

    created README and main_h file

---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+0x02 Learning C functions, protoptypes and nested loops
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,10 @@
+#ifndef main_h
+#define main_h
+/**
+* main - entry point of program
+* headers
+*
+* Return : always 0
+*/
+int _putchar (char a);
+#endif
