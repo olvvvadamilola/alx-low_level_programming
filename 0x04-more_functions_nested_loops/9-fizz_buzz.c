66e671a9e60286599ef5a136d6c77f0d1cc90a9d
Olvwadamilola <destric3@gmail.com>
2023-09-14 11:55:22 -0700

    9-fizz_buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,21 +11,25 @@ int main(void)
 
 	for (n = 1; n <= 100; n++)
 
-	if ((n % 3 == 0) && (n % 5 == 0))
+	if (n == 1)
 {
-	printf("FizzBuzz ");
+	printf("%d", n);
+}
+	else if ((n % 3 == 0) && (n % 5 == 0))
+{
+	printf(" FizzBuzz");
 }
 	else if (n % 3 == 0)
 {
-	printf("Fizz ");
+	printf(" Fizz");
 }
 	else if (n % 5 == 0)
 {
-	printf("Buzz ");
+	printf(" Buzz");
 }
 	else
 {
-	printf("%d ", n);
+	printf(" %d", n);
 }
 	printf("\n");
 	return (0);
