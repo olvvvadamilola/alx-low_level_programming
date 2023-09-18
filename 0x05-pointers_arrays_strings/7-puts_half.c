ee641d585ad359a7a2c545c5dae6f31ef5de086e
Olvwadamilola <destric3@gmail.com>
2023-09-18 12:08:15 -0700

    7puts

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -12,8 +12,16 @@ void puts_half(char *str)
 {
 	int length = strlen(str);
 	int i;
-	int start = length / 2;
+	int start;
 
+	if (length % 2 == 0)
+{
+	start = length / 2;
+}
+	else
+{
+	start = (length + 1) / 2;
+}
 	for (i = start; i < length; i++)
 {
 	_putchar(str[i]);
