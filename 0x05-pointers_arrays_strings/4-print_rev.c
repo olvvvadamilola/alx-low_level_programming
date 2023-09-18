1773a74e8c211157547b0267962ff1ace97c5dc3
Olvwadamilola <destric3@gmail.com>
2023-09-18 06:50:13 -0700

    even simpler way to reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -12,15 +12,11 @@ void print_rev(char *s)
 	int length = 0;
 	int i;
 
-	for (; *s != '\0'; s++)
-{
-	length++;
-}
-	s--;
-	for (i = length - 1; i >= 0; i--)
+	for (; *s != '\0'; length++, s++)
+{}
+	for (i = length; i > 0; i--, s--)
 {
 	_putchar(*s);
-	s--;
 }
 	_putchar('\n');
 }
