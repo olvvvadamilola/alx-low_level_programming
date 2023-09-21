54483af3b9dfcb81c4f7f7bb505665b9ae5da179
olvvvadamilola <destric3@gmail.com>
2023-09-21 01:08:53 +0100

    easier as expected

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -6,21 +6,22 @@
  * @dest: destination
  * @src: source
  * @n: integer
-*/
+ * Return: 0
+ */
 char *_strncat(char *dest, char *src, int n)
 {
-    int i;
-    int length = 0;
-    
-    while (dest[length] != '\0')
-    {
-        length++;
-    }
-        for (i = 0; i < n && src[i] != '\0'; i++)
-    {
-        dest[length] = src[i];
-        length++;
-    }
-    dest[length] = '\0';
-    return (dest);
+	int i;
+	int length = 0;
+
+	while (dest[length] != '\0')
+	{
+		length++;
+	}
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[length] = src[i];
+		length++;
+	}
+	dest[length] = '\0';
+	return (dest);
 }
