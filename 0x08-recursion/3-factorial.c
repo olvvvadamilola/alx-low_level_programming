232646e29b9a26cf8674063081ef1de9b7782590
olvvvadamilola <destric3@gmail.com>
2023-09-29 00:49:17 +0100

    factorials solving

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * factorial - function to print factorials
+ * @n: integer
+ * Return: 0
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+
+	else if (n == 0)
+	{
+		return (1);
+	}
+
+	else
+	{
+		return (n * factorial(n -1));
+	}
+}
