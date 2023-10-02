19f442d54066bde789029469d6d6a107e7dcf190
olvvvadamilola <destric3@gmail.com>
2023-10-02 21:21:23 +0100

    update 0x09

--- 0x09-static_libraries/1-strncat.c
+++ 0x09-static_libraries/1-strncat.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include "main.h"
-#include <string.h>
 /**
  * _strncat -concatenates two strings n times
  * @dest: destination
--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,10 +1,6 @@
 #ifndef main_h
 #define main_h
-#include <stdio.h>
-#include <string.h>
-#include <ctype.h>
-#include <stdlib.h>
-#include <unistd.h>
+
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
