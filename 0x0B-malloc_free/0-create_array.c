b33f159dbae2d9b61409992bdaeb5f82454d052e
olvvvadamilola <destric3@gmail.com>
2023-10-03 22:31:35 +0100

    create an array of characters and init

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,2 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "main.h"
+/**
+ * create_array - prototype to create an array of characters
+ * @size: size of array
+ * @c: specific character to be initialized
+ * Return: 0
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *ptr = malloc(size * sizeof(char));
+	unsigned int i;
+
+	if (size == 0 || ptr == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+	{
+		ptr[i] = c;
+	}
+	return (ptr);
+	free(ptr);
+}
