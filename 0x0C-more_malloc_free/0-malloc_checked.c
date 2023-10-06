2bf8a5e2e16924c28ee01a7da4f101e6038421b7
olvvvadamilola <destric3@gmail.com>
2023-10-07 00:05:59 +0100

    0-malloc_checked function to allocate memory and exit

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -2,15 +2,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * malloc_checked - allocates memory malloc and checks for errors
+ * malloc_checked - Allocates memory using malloc and checks for errors
  * @b: number of bytes to allocate
- * Return: pointer to allocated memory or 0 if it fails
- */
+ * Return: Pointer to allocated memory, or exit with status 98 if fails
+*/
 void *malloc_checked(unsigned int b)
 {
 	void *ptr;
 
 	ptr = malloc(b);
+    
 	if (ptr == NULL)
 	{
 		exit(98);
