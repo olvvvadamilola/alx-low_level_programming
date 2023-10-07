29d755379bc3d943383ecff806aa67cdf5b9b32b
olvvvadamilola <destric3@gmail.com>
2023-10-07 16:42:44 +0100

    realloc - reallocate memory

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -12,39 +12,40 @@
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	void *new_ptr;
-    unsigned int i;
+	char *new_ptr;
+	char *old_ptr;
+	unsigned int i;
 
 	if (new_size == old_size)
 		return (ptr);
 
-	if (ptr == NULL)
-	{
-		new_ptr = malloc(new_size);
-		if (new_ptr == NULL)
-			return (NULL);
-		return (new_ptr);
-	}
-
-	if (new_size == 0)
+	if (new_size == 0 && ptr)
 	{
 		free(ptr);
 		return (NULL);
 	}
 
+	if (!ptr)
+		return (malloc(new_size));
+
 	new_ptr = malloc(new_size);
-	if (new_ptr == NULL)
+	if (!new_ptr)
 		return (NULL);
 
-	if (new_size > old_size)
-		new_size = old_size;
+	old_ptr = ptr;
 
-	for (i = 0; i < new_size; i++)
+	if (new_size < old_size)
 	{
-		((char *)new_ptr)[i] = ((char *)ptr)[i];
+		for (i = 0; i < new_size; i++)
+			new_ptr[i] = old_ptr[i];
+	}
+
+	if (new_size > old_size)
+	{
+		for (i = 0; i < old_size; i++)
+			new_ptr[i] = old_ptr[i];
 	}
 
 	free(ptr);
-
 	return (new_ptr);
 }
