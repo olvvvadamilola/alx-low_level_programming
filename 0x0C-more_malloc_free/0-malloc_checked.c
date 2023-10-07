89e245e762f6dc9c63e679a0d59c2cb2b2a88649
olvvvadamilola <destric3@gmail.com>
2023-10-07 08:30:21 +0100

    llocates memory using malloc and checks for errors

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -6,13 +6,13 @@
  * malloc_checked - Allocates memory using malloc and checks for errors
  * @b: number of bytes to allocate
  * Return: Pointer to allocated memory, or exit with status 98 if fails
-*/
+ */
 void *malloc_checked(unsigned int b)
 {
 	void *ptr;
 
 	ptr = malloc(b);
-    
+
 	if (ptr == NULL)
 	{
 		exit(98);
