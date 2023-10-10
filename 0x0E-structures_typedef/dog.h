c5e97c37b75410b0c4febefa600861c24fee70a2
olvvvadamilola <destric3@gmail.com>
2023-10-11 00:21:49 +0100

    defined struct in header file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,17 +1,17 @@
 #ifndef DOG_H
 #define DOG_H
 /**
- * struct dog - Represents information about a dog.
- * @name: The name of the dog.
- * @age: The age of the dog.
- * @owner: The owner of the dog.
- * Return: 0
- */
+* struct dog - Represents information about a dog.
+* @name: The name of the dog.
+* @age: The age of the dog.
+* @owner: The owner of the dog.
+* Return: 0
+*/
 
 struct dog
 {
-    char *name;
-    float age;
-    char *owner;
+char *name;
+float age;
+char *owner;
 };
 #endif
