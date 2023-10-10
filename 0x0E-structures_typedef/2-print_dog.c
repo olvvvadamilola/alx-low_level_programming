c2b17e62dde67d92d253aef16b44f700f042a72a
olvvvadamilola <destric3@gmail.com>
2023-10-11 00:56:39 +0100

    function to handle NULLS

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+ * print_dog - prototype to print struct
+ * @d: pointer
+ * Return: 0
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+	{
+		return;
+	}
+
+	if (d->name == NULL)
+		d->name = "(nil)";
+	if (d->owner == NULL)
+		d->owner = "(nil)";
+
+	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+}
