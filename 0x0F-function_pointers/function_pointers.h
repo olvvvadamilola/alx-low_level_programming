3ad62c4b1a9274ec34bf6de4b2951bdb9d626356
olvvvadamilola <destric3@gmail.com>
2023-10-11 19:56:50 +0100

    function to print names

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,10 @@
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+/**
+ * main - Entry point
+ * description: header files
+ * Return: Always 0
+*/
+void print_name(char *name, void (*f)(char *));
+int _putchar(char c);
+#endif
