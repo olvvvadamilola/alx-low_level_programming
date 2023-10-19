0d52a3ff398042996ce21cb0aa372a2175ec28aa
olvvvadamilola <destric3@gmail.com>
2023-10-19 20:53:22 +0100

    print_list - prints all the elements of linkd list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * print_list - prints all the elements of a linked list
+ * @h: head of the linked list
+ *
+ * Return: No of elements in the linked list
+ */
+size_t print_list(const list_t *h)
+{
+	size_t n = 0;
+
+	while (h != NULL)
+	{
+		printf("[%d] %s\n", h->len, (h->str != NULL) ? h->str : "(nil)");
+		h = h->next;
+		n++;
+	}
+}
