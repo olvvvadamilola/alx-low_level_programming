42d294ab26bb8e078ff1491e3ce486202c1abd44
olvvvadamilola <destric3@gmail.com>
2023-10-23 17:02:01 +0100

    function to free list double pointer

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+/**
+ * free_listint2 - frees list
+ * @head: double pointer to head
+ * Return: 0
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *current, *temp;
+
+	if (head == NULL)
+		return;
+
+	current = *head;
+
+	while (current != NULL)
+	{
+		temp = current;
+		current = current->next;
+		free(temp);
+	}
+
+	*head = NULL;
+}
