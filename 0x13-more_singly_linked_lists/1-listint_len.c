997cc001c7236ce0707a79d3642dfedcc07df7fe
olvvvadamilola <destric3@gmail.com>
2023-10-23 15:28:51 +0100

    created function to print no of element in LL

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,4 +1,18 @@
 #include "lists.h"
 /**
- * 
-*/
+ * listint_len - returns the number of elements in a linked listint_t list.
+ * @h: pointer to the list
+ *
+ * Return: number of elements in the list
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t n = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		n++;
+	}
+	return (n);
+}
