9afe55cf011a9fbb03257ba799eb37559db7a5f7
olvvvadamilola <destric3@gmail.com>
2023-12-08 04:09:11 +0100

    fn that returns the sum of all the (n) of L list.

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - returns the sum of all the data (n) of a dlistint_t linked list
+ * @head: head of the list
+ *
+ * Return: sum
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+	while (head != NULL)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
+}
